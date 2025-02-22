1935ec779428ef775d89b1e1b5d91496e0ad0f44
Prabaha <dasprabaha23@icloud.com>
2025-02-22 18:49:28 +0530

    [sadbro] added init.sql

---
+++ init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       name TEXT NOT NULL,
+                       email TEXT NOT NULL UNIQUE,
+                       password TEXT NOT NULL,
+                       created_at TIMESTAMP DEFAULT NOW(),
+                       updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE tasks (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       title TEXT NOT NULL,
+                       description TEXT NOT NULL,
+                       hex_color TEXT NOT NULL,
+                       uid UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                       due_at TIMESTAMP DEFAULT (NOW() + INTERVAL '7 days'),
+                       created_at TIMESTAMP DEFAULT NOW(),
+                       updated_at TIMESTAMP DEFAULT NOW()
+);
