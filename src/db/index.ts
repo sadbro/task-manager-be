113776779920f74406643b09a695cb417b3490d4
Prabaha <dasprabaha23@icloud.com>
2025-02-24 20:35:52 +0530

    [sadbro] pg issue fix #fix5

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:test123@db:5432/task_manager",
+  connectionString: "postgresql://task_manager_db_1v1p_user:VvEbdNufAd7uslzHBXRlMPKxyHiAbCno@dpg-cuu8e8a3esus73eh0u2g-a.oregon-postgres.render.com/task_manager_db_1v1p",
 });
 
 export const db = drizzle(pool);
